4b7ad831d1362abcd07549170e29a9332dbc4d93
KushnerMikalai <nickolaikushner@gmail.com>
2019-08-20 23:19:28 +0300

    feat: add style and react-router

--- src/components/menu-banner/manu-banner.component.jsx
+++ src/components/menu-banner/manu-banner.component.jsx
@@ -2,14 +2,15 @@ import React from 'react';
 import './menu-banner.styles.scss';
 
 const MenuBanner = ({ title, imageUrl, size }) => (
-  <div
-    style={{
-      backgroundImage: `url(${imageUrl})`
-    }}
-   className={`${size ? size : ''} menu-banner`}
-  >
+  <div className={`${size ? size : ''} menu-banner`}>
+    <div
+      className="menu-banner__background"
+      style={{
+        backgroundImage: `url(${imageUrl})`
+      }}
+    />
     <div className="menu-banner__content">
-      <div className="menu-banner__title">{ title }</div>
+      <div className="menu-banner__title">{ title.toUpperCase() }</div>
       <div className="menu-banner__subtitle">shop now</div>
     </div>
   </div>
